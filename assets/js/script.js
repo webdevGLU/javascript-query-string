d797a0e6f963a50c208779ad0856fc622cb46d8b
Koen Borghols <kborghols@MacBook-Pro-van-Koen.local>
2022-11-03 13:13:04 +0100

    new edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,12 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
 // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-let voornaam = params.voornaam; // "some_value"
-let tussenvoegsel = params.tussenvoegsel; // "some_value"
-let achternaam = params.achternaam; // "some_value"
-document.write(voornaam);
-document.write(" "+tussenvoegsel+" ");
-document.write(achternaam);
+var voornaam = params.voornaam; // "voornaam"
+var tussenvoegsel = params.tussenvoegsel; // "tussenvoegsel"
+var achternaam = params.achternaam; // "achternaam"
+var straat = params.straat; // "achternaam"
+
+document.write(voornaam+" "+tussenvoegsel+" "+achternaam);
+document.write("<br>"+straat);
+// document.write(" "+tussenvoegsel+" ");
+// document.write(achternaam);
